38a656213e429d5738d875ceec7cd5f2cbd09e80
silvasantosrafael <rafael.silva318819@gmail.com>
2023-03-07 09:00:37 -0300

    Added new method for fetch pokemon data and documentation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 const imgSearch = document.querySelector('#img-search')
 const inputText = document.querySelector('#input-text')
 
+/**
+ * Método que obtém os dados do pokemon
+ * 
+ * @param value Nome ou ID do pokemon
+ * @returns Retorna os dados do pokemon
+ */
+const fetchPokemon = async value => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${value}`)
+  const pokemonData = response.json()
 
-imgSearch.addEventListener('click', () => {
-  console.log(inputText.value);
+  return pokemonData
+}
+
+/**
+ * Listener de evento na imagem de lupa para capturar
+ * o nome do pokemon ou ID
+ */
+imgSearch.addEventListener('click', async () => {
+  const pokemon = await fetchPokemon(inputText.value)
 })
+
