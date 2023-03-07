db823635def3b04d7cbcf35d89176036be99588d
silvasantosrafael <rafael.silva318819@gmail.com>
2023-03-07 00:06:23 -0300

    minor fix and added script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const imgSearch = document.querySelector('#img-search')
+const inputText = document.querySelector('#input-text')
+
+
+imgSearch.addEventListener('click', () => {
+  console.log(inputText.value);
+})
