359659adfe7906700bcac4ba4aeadc1e801ea406
silvasantosrafael <rafael.silva318819@gmail.com>
2023-03-09 00:31:22 -0300

    Added new methods, minor fix in css and documentation

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ const pokemonName = document.querySelector('#pokemon-name')
 const pokemonId = document.querySelector('#pokemon-id')
 const imagePlaceholder = document.querySelector('#image-placeholder')
 const types = document.querySelector('#types')
+const stats = document.querySelector('#stats')
+const pokedex = document.querySelector('#pokedex')
 
+const MAX_STAT_VALUE = 255
 
 /**
  * Método que obtém os dados do pokémon
@@ -23,24 +26,22 @@ const fetchPokemon = async value => {
 }
 
 /**
+ * Método que define a cor de destaque baseado no primeiro Tipo do pokémon
+ * 
+ * @param pokemon Dados do pokemon obtidos da API
+ */
+const setPokedexAccentColor = pokemon => {
+  pokedex.style.backgroundColor = `var(--${pokemon.types[0].type.name})`
+}
+
+/**
  * Método que insere e formata o nome e ID do pokémon
  * 
  * @param pokemon Dados do pokemon obtidos da API
  */
-const insertNameAndIDPokemon = pokemon => {
+const insertNameAndIdPokemon = pokemon => {
   pokemonName.innerText = pokemon.name.toUpperCase()
-  pokemonId.innerText = '#' + pokemon.id.toString().padStart(3, 0)
-}
-
-/**
- * Método que formata o texto obtido no input
- * 
- * @returns Texto formatado digitado no input
- */
-const formattedInputText = () => {
-  const value = inputText.value.toLowerCase().split(' ').join('-')
-
-  return value
+  pokemonId.innerText = '#' + formattedNumeral(pokemon.id)
 }
 
 /**
@@ -75,6 +76,42 @@ const insertPokemonTypes = pokemon => {
 }
 
 /**
+ * Método que calcula o valor correto para inserir
+ * 
+ * @param value Valor base stat do pokémon
+ * @returns Retorna o valor calculado
+ */
+const calculateStatValue = value => {
+  const baseValue = MAX_STAT_VALUE
+  const calculateValue = (value / baseValue) * 100
+  const valueCalculated = Math.round(calculateValue)
+
+  return valueCalculated
+}
+
+/**
+ * Método que insere as estatísticas básicas do pokémon
+ * 
+ * @param pokemon Dados do pokémon obtidos da API
+ */
+const insertBaseStats = pokemon => {
+  stats.innerHTML = `<h3 id="base-stats" style="color: var(--${pokemon.types[0].type.name})">Base Stats</h3>`
+  pokemon.stats.forEach(st => {
+    const templateHTML = 
+    `<div class="stat-row">
+      <div class="stat-desc" style="color: var(--${pokemon.types[0].type.name})">${st.stat.name.toUpperCase()}</div>
+        <div class="stat-value">${formattedNumeral(st.base_stat)}</div>
+          <div class="stat-bar">
+            <div class="bar-outer" style="background-color: var(--${pokemon.types[0].type.name +'Alpha'})">
+            <div class="bar-inner" style="width: ${calculateStatValue(st.base_stat)}%; background-color: var(--${pokemon.types[0].type.name})"></div>
+        </div>
+      </div>
+    </div>`
+    stats.innerHTML += templateHTML
+  })
+}
+
+/**
  * Método que verifica se o campo texto input está vazio
  * 
  * @param text Texto digitado no input
@@ -90,6 +127,27 @@ const validateText = text => {
 }
 
 /**
+ * Método que formata o numero para 3 casas decimais
+ * 
+ * @param number Número que será formatado
+ * @returns Retorna o numero no formato 000
+ */
+const formattedNumeral = number => {
+  return number.toString().padStart(3, 0) 
+}
+
+/**
+ * Método que formata o texto obtido no input
+ * 
+ * @returns Texto formatado digitado no input
+ */
+const formattedInputText = () => {
+  const value = inputText.value.toLowerCase().split(' ').join('-')
+
+  return value
+}
+
+/**
  * Listener de evento na imagem de lupa para capturar
  * o nome do pokémon ou ID
  */
@@ -98,8 +156,11 @@ imgSearch.addEventListener('click', async () => {
   if (!validateText(value)) {
     return
   }
+  inputText.value = ''
   const pokemon = await fetchPokemon(value)
-  insertNameAndIDPokemon(pokemon)
+  setPokedexAccentColor(pokemon)
+  insertNameAndIdPokemon(pokemon)
   insertPokemonImage(pokemon)
   insertPokemonTypes(pokemon)
+  insertBaseStats(pokemon)
 })
